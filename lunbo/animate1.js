1358b8da461cc47ce06d9dfc94eb5063867d07db
lijianhui1997 <1774407914@qq.com>
2018-11-11 19:14:32 +0800

    提交

---
+++ lunbo/animate1.js
@@ -0,0 +1,74 @@
+window.onload = function() {
+    var container = document.getElementById('container');
+    var list = document.getElementById('list');
+    var buttons = document.getElementById('buttons').getElementsByTagName('span');
+    var prev = document.getElementById('prev');
+    var next = document.getElementById('next');
+    var index = 1;
+    var timer;
+
+function animate(offset) {
+        var newLeft = parseInt(list.style.left) + offset;
+        list.style.left = newLeft + 'px';
+        if (newLeft > -1200) {
+            list.style.left = -6000 + 'px';
+        }
+        if (newLeft < -6000) {
+            list.style.left = -1200 + 'px';
+        }
+    }
+
+    function play() {
+        timer = setInterval(function() {
+            next.onclick();
+        }, 2000)
+    }
+
+    function stop() {
+        clearInterval(timer);
+    }
+
+    function buttonsShow() {
+        for (var i = 0; i < buttons.length; i++) {
+            if (buttons[i].className == "on") {
+                buttons[i].className = "";
+            }
+        }
+        buttons[index - 1].className = "on";
+    }
+
+    prev.onclick = function() {
+        index -= 1;
+        if (index < 1) {
+            index =9;
+        }
+        buttonsShow();
+        animate(1200);
+    };
+
+    next.onclick = function() {
+        index += 1;
+        if (index > 9) {
+            index = 1
+        }
+        animate(-1200);
+        buttonsShow();
+    };
+
+    for (var i = 0; i < buttons.length; i++) {
+        (function(i) {
+            buttons[i].onclick = function() {
+                var clickIndex = parseInt(this.getAttribute('index'));
+                var offset = 1200 * (index - clickIndex); 
+                animate(offset);
+                index = clickIndex; 
+                buttonsShow();
+            }
+        })(i)
+    }
+
+    container.onmouseover = stop;
+    container.onmouseout = play;
+    play();
+
+}
